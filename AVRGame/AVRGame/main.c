b7a74628b734dc0ffb573e8124d61678f732b81b
TorFJ <torfrostrup@gmail.com>
2018-04-19 11:39:55 +0200

    Project created

---
+++ AVRGame/AVRGame/main.c
@@ -0,0 +1,18 @@
+/*
+ * AVRGame.c
+ *
+ * Created: 19.04.2018 11:34:44
+ * Author : Tor
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
